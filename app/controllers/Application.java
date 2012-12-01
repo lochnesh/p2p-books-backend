db2fd83805cda22de434f682cb6eacd30c6acaa8
Skyler Nesheim <skylernesheim@gmail.com>
2012-12-01 13:49:30 -0600

    returning multiple books

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,6 +8,8 @@ import static play.libs.Json.toJson;
 import views.html.*;
 import models.*;
 
+import java.util.*;
+
 public class Application extends Controller {
   
   public static Result index() {
@@ -15,12 +17,20 @@ public class Application extends Controller {
   }
 
   public static Result books() {
+    return ok(toJson(getBooks()));
+  }
+
+  private static List<Book> getBooks() {
+    List<Book> booksList = new ArrayList<Book>();
     Book book = new Book();
     book.setTitle("Our Book");
     book.setAuthor("Dwolla Dwolla");
     book.setListPrice(100.00);
     book.setPublisher("Testing Publisher");
-    return ok(toJson(book));
+    booksList.add(book);
+    booksList.add(book);
+    return booksList;
   }
+    
   
 }
